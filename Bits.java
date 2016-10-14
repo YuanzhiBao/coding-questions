1a8f275f273fed03a26f52828edd376a458aefd1
baoyu <baoyu@indiana.edu>
2016-10-14 17:09:40 -0400

    bit manipulation

--- Bits.java
+++ Bits.java
@@ -3,6 +3,15 @@ public class Solution {
     public int hammingWeight(int n) {
         int count = 0;
         //while (n != 0)
+        //when we implement 2^31 = 2147483648 
+        //it is 2147483647 + 1
+        //which is 01111111 11111111 11111111 11111111 + 00000000 00000000 00000000 00000001
+        //it become 10000000 00000000 00000000 00000000 in the system.
+        //which is -2147483648, as we right shift this number, it add 1 to the top.
+        //so it will become 11000000 00000000 00000000 00000000 for the first move
+        //11100000 00000000 00000000 00000000 for the second move
+        //And keep going like this way
+        //for loop is right,  but when you use while loop it become funny.
         for (int i = 0; i < 32; i++ )
         {
            System.out.println(count);
